a00862033eab68eb846754c656dfdb9d32cd39eb
tana-16 <tanahiro1697@gmail.com>
2021-07-12 13:05:42 +0000

    add2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,51 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
-    # １. データを新規登録するためのインスタンス作成
+    @books = Book.all
+    @book = Book.new
     book = Book.new(book_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = "errors"
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = "errors"
+      render :show
+    end
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   private
   # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'books/new'
+  # get 'books/new'
   get '//' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_books'
-  patch 'books/:id' => 'books#update',as:'update_todolist'
-  
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_books'
+  # patch 'books/:id' => 'books#update',as:'update_todolist'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_books'
+  resources :books
 end
